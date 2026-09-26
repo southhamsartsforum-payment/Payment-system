a2c5740413f84908a4b19c3cb40a1b23e2a7feba
Steve Dooley <Southhamsartsforum@gmail.com>
2026-09-26 13:37:17 +0100

    Code corrections

--- SHAF/Visitor_Counter/functions/index.js
+++ SHAF/Visitor_Counter/functions/index.js
@@ -8,8 +8,8 @@
  */
 
 const {setGlobalOptions} = require("firebase-functions");
-//const {onRequest} = require("firebase-functions/https");
-//const logger = require("firebase-functions/logger");
+// const {onRequest} = require("firebase-functions/https");
+// const logger = require("firebase-functions/logger");
 
 // For cost control, you can set the maximum number of containers that can be
 // running at the same time. This helps mitigate the impact of unexpected
